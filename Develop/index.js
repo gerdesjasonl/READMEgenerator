d4bdfa2b35fc8ae5d33fe898585360a4587d044f
gerdesjasonl <gerdesjasonl@gmail.com>
2025-01-20 09:33:23 -0500

    This makes functional the README generator along with draft entries in readmeText.html for video test.

--- Develop/index.js
+++ Develop/index.js
@@ -4,7 +4,9 @@ import colors from 'colors';
 import fs from 'fs';
 
 // TODO: Create an array of questions for user input
-const userResponses = ({userInstall, userUsage, userLicense, userContributing, userTests, userQuestions}) =>
+// TODO: Create a function to write README file
+   
+const userResponses = ({userTitle, userDescription, userInstall, userUsage, userLicense, userContribGuide, userTests, userGitHub, userEmail}) =>
     `<!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,83 +16,102 @@ const userResponses = ({userInstall, userUsage, userLicense, userContributing, u
 </head>
 <header>
  <h1 style="background-color: black;text-align: center;color:azure;">${userTitle}</h1>
- <h3 id="projDesc">${userDescription}</h3>
+ <h3 style="text-align: center" id="projDesc">${userDescription}</h3>
 </header>
 <body>
     <h2 style="background-color: black;text-align: center;color:azure;">Table of Contents</h2>
-    <ul id="tableContents">
+    <ul id="tableContents" style="text-align: center">
         <a href="#installation">Installation</a>
         <a href="#usage">Usage</a>
         <a href="#license">License</a>
-        <a href="#contributing">Contributing</a>
+        <a href="#contribGuide">Contributing</a>
         <a href="#tests">Tests</a>
         <a href="#questions">Questions</a>
     </ul>
-
-    <h3 style="background-color: black;text-align: center;color:azure;" id="installation">Installation</h3>
-        <p id="installEntry">${userInstall}</p>
-    <h3 style="background-color: black;text-align: center;color:azure;" id="usage">Usage</h3>
-        <p id="usageEntry">${userUsage}</p>
+    <h3 style="background-color: black;text-align: center;color:azure;" id="installation">Installation Instructions</h3>
+    <p id="installEntry">${userInstall}</p>
+    <h3 style="background-color: black;text-align: center;color:azure;" id="usage">Usage Information</h3>
+    <p id="usageEntry">${userUsage}</p>
     <h3 style="background-color: black;text-align: center;color:azure;" id="license">License</h3>
-        <p id="licenseEntry">${userLicense}</p>
-    <h3 style="background-color: black;text-align: center;color:azure;" id="contributing">Contributing</h3>
-        <p id="contribEntry">${userContributing}</p>
-    <h3 style="background-color: black;text-align: center;color:azure;" id="tests">Tests</h3>
-        <p id="testsEntry">${userTests}</p>
-    <h3 style="background-color: black;text-align: center;color:azure;" id="questions">Questions</h3>
-        <p id="questionsEntry">${userQuestions}</p>
-    <footer>
-
-    </footer>
+    <p id="licenseEntry">${userLicense}</p>
+    <h3 style="background-color: black;text-align: center;color:azure;" id="contribGuide">Contribution Guidelines</h3>
+    <p id="contribEntry">${userContribGuide}</p>
+    <h3 style="background-color: black;text-align: center;color:azure;" id="tests">Test Instructions</h3>
+    <p id="testsEntry">${userTests}</p>
+    <h3 style="background-color: black;text-align: center;color:azure;" id="questions">Fuurther Questions</h3>
+    <p id="questionsEntry">${userGitHub} ${userEmail}</p>
 </body>
+<footer>
+
+</footer>
+
 </html>`;
 
-inquirer
+// const initReadmeGen = {}
 
+inquirer
     .prompt([
         {
-            type: 'input',
-            message: colors.bgCyan('Enter Install Information'),
-            name: 'userInstall',
+            type:'text',
+            name:'userTitle',
+            message: colors.bgCyan('Enter Title for README')   
         },
-        {
-            type: 'input',
-            message: colors.bgCyan('Enter Usage Information'),
-            name: 'userUsage',
-        },
-        {
-            type: 'input',
-            message: colors.bgCyan('Enter License Information'),
-            name: 'userLicense',
-        },
-        {
-            type: 'input',
-            message: colors.bgCyan('Enter Contributing Credits'),
-            name: 'userContributing',
-        },
-        {
-            type: 'input',
-            message: colors.bgCyan('Describe Tests for Program'),
-            name: 'userTests',
-        },
-        {
-            type: 'input',
-            message: colors.bgCyan('Enter Concluding Questions'),
-            name: 'userQuestions',
-        },
-    ]) 
 
-// TODO: Create a function to write README file
-.then((answers) => {
+        {
+            type:'text',
+            name:'userDescription',
+            message: colors.bgCyan('Enter Project Description')
+        },
+
+        {
+            type:'text',
+            name:'userInstall',
+            message: colors.bgCyan('Enter Install Information')
+        },
+
+        {
+            type:'text',
+            name:'userUsage',
+            message: colors.bgCyan('Enter Usage Information')   
+        },
+
+        {
+            type:'text',
+            name:'userLicense',
+            message: colors.bgCyan('Enter License Information')  
+        },
+
+        {
+            type: 'text',
+            name:'userContribGuide',
+            message: colors.bgCyan('Enter Contribution Guidelines')   
+        },
+
+        {
+            type:'text',
+            name:'userGitHub',
+            message: colors.bgCyan('Enter GitHub Address') 
+        },
+
+        {
+            type:'text',
+            name:'userEmail',
+            message: colors.bgCyan('Enter Email Address')
+        },
+    ])
+
+    .then((answers) => {
     const userReadme = userResponses(answers);
-    
-    fs.writeToFile('README.md', userReadme, (err) =>
+    fs.writeFile('README.md', userReadme, (err) =>
     err ? console.log(err) : console.log ('Succesfully created README.md')
-);
-});
+    );
+    });
+
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+// initReadmeGen();
+}
 
 // Function call to initialize app
 init();
